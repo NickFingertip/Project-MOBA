5162f72402bfdd3845f31d154668add117b8285e
EnkiLonde <enkilonde@gmail.com>
2015-03-06 19:55:49 +0100

    initial project - fps controller

---
+++ MOBA_FPS/Assets/CharacterControllerScript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterControllerScript : MonoBehaviour {
+
+	public float speed = 6.0F;
+	private float actualSpeed;
+	private bool dashing;
+	public float dashTime = 0.35f;
+	private float _dashTime;
+	public float jumpSpeed = 8.0F;
+	public float dashSpeed = 20.0f;
+	public float gravity = 20.0F;
+	public int jumpNumber = 1;
+	private int jumpCount = 0;
+	private Vector3 moveDirection = Vector3.zero;
+	private bool jumping = false;
+
+	private CharacterController controller;
+	
+	void Start(){
+		controller = GetComponent<CharacterController>();
+		actualSpeed = speed;
+
+		//Cursor.visible = false;
+
+	}
+	
+	void Update() {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		if (Input.GetKeyDown (KeyCode.Escape))
+			Application.LoadLevel (0);
+
+
+		if (Input.GetKeyDown(KeyCode.V) && !dashing) {
+			_dashTime = dashTime;
+			dashing = true;
+		}
+
+		if(dashing){
+			actualSpeed = dashSpeed;
+			_dashTime -= Time.deltaTime;
+
+			if(_dashTime <= 0) dashing = false;
+		}
+		else{
+			actualSpeed = speed;
+		}
+
+		if (controller.isGrounded) {
+			jumpCount = 0;
+
+			moveDirection = new Vector3(inputX, 0, inputY);
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= actualSpeed;
+
+			if(jumping){
+				jumping = false;
+			}
+
+		}
+		else{
+			jumping = true;
+
+			moveDirection.x = inputX * actualSpeed;
+			moveDirection.z = inputY * actualSpeed;
+			moveDirection = transform.TransformDirection(moveDirection);
+		}
+		
+		if (Input.GetButtonDown("Jump")) {
+			if(jumpCount < jumpNumber){
+				jumpCount++;
+				moveDirection.y = jumpSpeed;
+			}
+		}
+		
+		moveDirection.y -= gravity * Time.deltaTime;
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
---
+++ MOBA_FPS/Assets/Scripts/RandomMatchmaker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMatchmaker : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("0.5");
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+	
+	void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+	
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom ("Room1");
+	} 
+	
+	void OnJoinedRoom ()
+	{
+		Debug.Log ("Room Joined");
+		
+		//monster.GetComponent<myThirdPersonController>().isControllable = true;
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
